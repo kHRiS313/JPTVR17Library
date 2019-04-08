4c74022e1a9e7f2c817c1fa149bfc23787e8e4db
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2019-04-08 13:55:45 +0300

    Создан консольный интерфейс программы в классе App

--- src/jptvr17library/App.java
+++ src/jptvr17library/App.java
@@ -5,14 +5,50 @@
  */
 package jptvr17library;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Melnikov
  */
-class App {
+public class App {
 
-    void run() {
-        
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Наша библиотека ----");
+        String repeat = "r";
+        int operation;
+        do{
+            System.out.println("Выберите действие:");
+            System.out.println("1. Создать книгу");
+            System.out.println("2. Создать читателя");
+            System.out.println("3. Выдать книгу читателю");
+            System.out.println("4. Читатель возвращает книгу");
+            operation = scanner.nextInt();
+            scanner.nextLine();
+            switch (operation) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;    
+                default:
+                    System.out.println("Такое действие неподдерживается");
+                    continue;
+            }
+            System.out.println("Для продолжения программы");
+            System.out.println("введите \"r\"");
+            System.out.println("Для окончания \"q\"");
+            repeat = scanner.nextLine();
+        }while("r".equals(repeat));
+        System.out.println("Программа закрывается");
     }
     
 }
